69981a31bf38d8795b24224858e5994b0bfe0af9
Aleksandr Smirnov <alesmi@ttu.ee>
2022-07-20 10:36:32 +0300

    Add server and endpoints.

---
+++ server/server.py
@@ -0,0 +1,23 @@
+import json
+import os
+
+from flask import Flask, send_from_directory
+
+app = Flask(__name__)
+
+
+@app.route('/users')
+def users():
+    return send_from_directory('api', 'users.txt')
+
+
+@app.route('/gists/<username>')
+def gists(username):
+    return send_from_directory('api', f'gists/{username}.txt')
+
+
+if __name__ == "__main__":
+    env = os.environ.get('APP_ENV', 'development')
+    port = int(os.environ.get('PORT', 5000))
+    debug = False if env == 'production' else True
+    app.run(host='0.0.0.0', port=port, debug=debug)
