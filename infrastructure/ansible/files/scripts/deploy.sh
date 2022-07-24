cbe78182fad27bf61894c19b253169725ce8d742
Aleksandr Smirnov <aleksandr.smirnov@merada.ee>
2022-07-24 18:25:27 +0300

    Add deploy step and update deploy script.

--- infrastructure/ansible/files/scripts/deploy.sh
+++ infrastructure/ansible/files/scripts/deploy.sh
@@ -5,9 +5,20 @@ if [ -z "$1" ]; then
   echo "Missing VERSION_TAG"
   exit 2
 fi
+if [ -z "$2" ]; then
+  echo "Missing DOCKER_USERNAME"
+  exit 2
+fi
+if [ -z "$3" ]; then
+  echo "Missing DOCKER_PASSWORD"
+  exit 2
+fi
+
 
 
 VERSION_TAG=$1
+DOCKER_USERNAME=$2
+DOCKER_PASSWORD=$3
 
 cd /home/ubuntu/docker/gist-scanner
 
@@ -18,6 +29,8 @@ function update_env {
 
 }
 
+# docker login
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 update_env .env
 sudo docker-compose down
 sudo docker-compose up -d
